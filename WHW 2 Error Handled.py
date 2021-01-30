7f762fd66ce31aaa5732908617101e440674fbb2
LesherG <63009369+LesherG@users.noreply.github.com>
2021-01-30 15:25:49 -0800

    Add files via upload

---
+++ WHW 2 Error Handled.py
@@ -0,0 +1,21 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("\nPlease enter the year to be evaluated as the 1st arguement\n[py \"Written HW 1.py\" 2021]")
+    sys.exit()
+    
+if not sys.argv[1].isdigit():
+    print("\nThe number you entered is not a year. Please try again.\n")  
+    sys.exit()
+    
+year = int(sys.argv[1])
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(str(year) + " is a leap year")
+            sys.exit()
+    else:
+        print(str(year) + " is a leap year")
+        sys.exit()
+
+print(str(year) + " is not a leap year")
